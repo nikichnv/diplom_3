5df314faf48500bc85ab0e592ab2ac72c1d92dfa
Nikita Sapelnikov <sapelnickovn@yandex.ru>
2024-02-08 00:42:08 +0300

    Реализовал pageobject для тестов

---
+++ src/main/java/pageobject/RecoveryPassPage.java
@@ -0,0 +1,31 @@
+package pageobject;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static config.RestConfig.FORGOT_PASSWORD;
+
+public class RecoveryPassPage {
+
+    private final By buttonSignInLocator = By.xpath("//*/div/p/a[text()='Войти']");
+    private WebDriver webDriver;
+
+    public RecoveryPassPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    @Step("Открыть страницу восстановления пароля ")
+    public void openPage() {
+        webDriver.get(FORGOT_PASSWORD);
+    }
+
+    @Step("Выполнить переход на страницу входа нажав на кнопку Войти")
+    public void clickButtonSignIn() {
+        WebElement buttonSignIn = webDriver.findElement(buttonSignInLocator);
+        buttonSignIn.click();
+    }
+
+
+}
